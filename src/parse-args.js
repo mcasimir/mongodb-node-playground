4e4f702d514458433c4221bf4df55b99dcfc41c6
mcasimir <maurizio.cas@gmail.com>
2020-10-05 19:16:30 +0200

    simplify connection string building

--- src/parse-args.js
+++ src/parse-args.js
@@ -1,4 +1,29 @@
-export function parseArgs(args) {
-  const connectionString = args[0] || 'mongodb://localhost:27017';
-  return { connectionString };
+const yargsParser = require('yargs-parser');
+
+function usage() {
+  console.info('USAGE: mongodb-node-playground [url] [options]');
+  process.exit(1);
 }
+
+export function parseArgs(argv) {
+  const args = yargsParser(argv);
+
+  if (args._.length > 1) {
+    usage();
+  }
+
+  const connectionStringUrl = new URL(args._[0] || 'mongodb://localhost:27017');
+
+  const queryStringOptions = Object.entries(args)
+    .filter(([name]) => name != '_')
+    .map(([name, value]) => [name, value.toString()]);
+
+  for (const [name, value] of queryStringOptions) {
+    connectionStringUrl.searchParams.append(name, value);
+  }
+
+  return {
+    connectionString: connectionStringUrl.toString()
+  };
+}
+
